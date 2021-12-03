3fc87e6e11ce0c5ef17739d575f133db1b8972fd
Danhtran123 <47044112+Danhtran123@users.noreply.github.com>
2021-12-03 18:02:45 -0500

    fixed registration servlet to respond to unregistered users

--- src/main/java/com/revature/durcweb/web/servlets/RegistrationServlet.java
+++ src/main/java/com/revature/durcweb/web/servlets/RegistrationServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class RegistrationServlet extends HttpServlet {
 
@@ -32,7 +33,10 @@ public class RegistrationServlet extends HttpServlet {
         resp.setContentType("application/json");
         try {
             NewUserRequest newUser = mapper.readValue(req.getInputStream(), NewUserRequest.class);
-            userService.registerUser(newUser);
+            if(!userService.registerUser(newUser)) {
+                resp.setStatus(400);
+                return;
+            }
             logger.info("Registered new user: {}", newUser.getUsername());
             resp.setStatus(201);
         } catch (InvalidRequestException | JsonParseException | UnrecognizedPropertyException e) {
